5e690f36b3d1b44bb2b599d1b22b435a5142e282
Seungjin Lee <quack337@gmail.com>
2016-10-03 21:57:56 +0900

    두번째 버전 ("02 master detail report.docx")

--- src/main/java/helloReport/HelloController.java
+++ src/main/java/helloReport/HelloController.java
@@ -1,22 +1,21 @@
 package helloReport;
 
 import java.io.IOException;
-import java.util.List;
-
+import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import net.sf.jasperreports.engine.JRException;
 
 @Controller
 public class HelloController {
     
     @Autowired StudentMapper studentMapper;
+    @Autowired SimpleDriverDataSource dataSource;
     
     @RequestMapping("/studentList1.do")
     public String studentList1() {
@@ -30,9 +29,11 @@ public class HelloController {
     
     @RequestMapping("/report/studentList.do")
     public void studentListReport(@RequestParam("did") int did, @RequestParam("type") String type, 
-            HttpServletRequest request, HttpServletResponse response) throws JRException, IOException {
-        List<Student> list = studentMapper.selectByDepartmentId(did);
-        ReportBuilder reportBuilder = new ReportBuilder("studentList", list, request, response);
+            HttpServletRequest request, HttpServletResponse response) throws JRException, IOException, SQLException {
+        ReportBuilder reportBuilder = new ReportBuilder("studentList", request, response);
+        reportBuilder.setConnection(dataSource.getConnection());
+        reportBuilder.setParameter("departmentId", did);
+        reportBuilder.addSubReport("coursesOfStudent.jasper");
         reportBuilder.build(type);
     }
 }
--- src/main/java/helloReport/ReportBuilder.java
+++ src/main/java/helloReport/ReportBuilder.java
@@ -1,24 +1,20 @@
 package helloReport;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.JasperRunManager;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.HtmlExporter;
 import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
@@ -31,22 +27,42 @@ import net.sf.jasperreports.export.type.HtmlSizeUnitEnum;
 
 public class ReportBuilder {
     static String reportFolderPath;
-    String reportFileName;
-    JRDataSource dataSource;
     HttpServletRequest request;
     HttpServletResponse response;
     JasperReport report;
+    List<String> subReports;
+    Connection connection;
+    Map<String,Object> params = new HashMap<String,Object>();
     
-    public ReportBuilder(String reportFileName, Collection<?> collection, 
+    public ReportBuilder(String reportFileName, 
             HttpServletRequest request, HttpServletResponse response) throws JRException {
-        this.reportFileName = reportFileName;
-        this.dataSource = new JRBeanCollectionDataSource(collection);
         this.request = request;
         this.response = response;        
         if (reportFolderPath == null)
             reportFolderPath = request.getSession().getServletContext().getRealPath("/WEB-INF/report");
         String reportFilePath = reportFolderPath + "/" + reportFileName + ".jasper";
         this.report = (JasperReport)JRLoader.loadObjectFromFile(reportFilePath);
+    }
+    
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+    
+    public void addSubReport(String fileName) {
+        if (subReports == null) subReports = new ArrayList<String>();
+        subReports.add(fileName);
+    }
+    
+    public void setParameter(String name, Object value) {
+        params.put(name, value);
+    }
+    
+    private void addSubReportToParams(Map<String,Object> params) {
+        if (subReports != null && subReports.size() > 0)
+            for (String s : subReports) {
+                String path = reportFolderPath + "/" + s;
+                params.put(s, path);
+            }        
     }
     
     public void build(String type) throws JRException, IOException {
@@ -59,9 +75,9 @@ public class ReportBuilder {
     
     public void buildHtmlReport() throws JRException, IOException {
         response.setCharacterEncoding("UTF-8");
-        Map<String,Object> params = new HashMap<String,Object>();
         params.put(JRParameter.IS_IGNORE_PAGINATION, true);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        addSubReportToParams(params);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, connection);
         List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
         jasperPrintList.add(jasperPrint);
         HtmlExporter exporter = new HtmlExporter();
@@ -77,9 +93,9 @@ public class ReportBuilder {
     
     public void buildXlsxReport() throws IOException, JRException {
         response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");        
-        Map<String,Object> params = new HashMap<String,Object>();
         params.put(JRParameter.IS_IGNORE_PAGINATION, true);        
-        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        addSubReportToParams(params);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, connection);
         List<JasperPrint> jasperPrintList = new ArrayList<JasperPrint>();
         jasperPrintList.add(jasperPrint);
         JRXlsxExporter  exporter = new JRXlsxExporter (); 
@@ -98,8 +114,9 @@ public class ReportBuilder {
     }
     
     public void buildPDFReport() throws JRException, IOException {
+        addSubReportToParams(params);
         byte[] bytes = null;
-        bytes = JasperRunManager.runReportToPdf(report, null, dataSource);
+        bytes = JasperRunManager.runReportToPdf(report, params, connection);
         response.setContentType("application/pdf");        
         response.setContentLength(bytes.length);
         ServletOutputStream ouputStream = response.getOutputStream();
